3013e5eb2edd446615f259b1575b2849e1687666
Felipe da Silva Gonçalves <felipedsg.work@gmail.com>
2026-03-04 21:10:51 -0300

    feat: implement gradient background layout

--- public/index.php
+++ public/index.php
@@ -5,12 +5,16 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="assets/css/style.css">
     <script defer src="assets/js/script.js"></script>
 </head>
 
 <body>
-    <h1>teste</h1>
+    <h1>Login</h1>
+    <h2>Bem vindo Felipe!</h2>
     <nav>
         <a href="index.php">Home</a> |
         <a href="dashboard.php">Dashboard</a> |

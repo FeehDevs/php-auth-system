1b8853d5e2084b6286e1a8dc6b5ca349441f9319
Felipe da Silva Gonçalves <felipedsg.work@gmail.com>
2026-03-30 19:30:53 -0300

    Fix: atualização do docker-compose

--- backend/models/user.php
+++ backend/models/user.php
@@ -1,13 +1,15 @@
 <?php 
 
-    require_once "./backend/database/conexao.php";
+// __DIR__ é uma constante mágica do PHP que retorna o diretório atual do arquivo
+    require_once __DIR__ . "/../database/conexao.php";
 
     class Usuario {
         private $pdo;
 
         public function __construct($pdo)
         {
-            
-            
+            $this->pdo = $pdo;
         }
+
+        
     }
